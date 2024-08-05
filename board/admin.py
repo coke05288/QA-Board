bd16a7ce0ef4cfe82e8c0aee9319968a9134714c
coke05288 <coke05288@gmail.com>
2024-08-05 17:07:53 +0900

    20240805 MON | add model & list view

--- board/admin.py
+++ board/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Question, Answer
 
-# Register your models here.
+class QuestionAdmin(admin.ModelAdmin):
+    search_fields = ['subject']
+
+class AnswerAdmin(admin.ModelAdmin):
+    search_fields = ['content']
+
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Answer, AnswerAdmin)
--- board/views.py
+++ board/views.py
@@ -1,4 +1,12 @@
-from django.http import HttpResponse
+from django.shortcuts import render
+from .models import Question
+
 
 def index(request):
-    return  HttpResponse("Welcome Board APP!")
+    question_list = Question.objects.order_by('-created_date')
+    
+    context = {
+        'question_list': question_list
+    }
+
+    return render(request, 'board/question_list.html', context)
